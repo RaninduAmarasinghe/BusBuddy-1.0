8f2e05e50075bf3448a9ac06d9e981c9c65e6dbb
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2026-06-05 20:44:50 +0530

    add new endpoint at securityconfig

--- src/main/java/com/busbuddy/busbuddy/config/SecurityConfig.java
+++ src/main/java/com/busbuddy/busbuddy/config/SecurityConfig.java
@@ -81,6 +81,7 @@ public class SecurityConfig {
 
                                 "/driver/login",
                                 "/driver/add",
+                                "/driver/search"
 
                                 "/bus/add",
                                 "/bus/company/**",
